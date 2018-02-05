54bfb5e0dd07abdfa03b458836b7381e9e8cab23
Edward Norton <edwardnorton@icloud.com>
2018-02-05 22:57:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 katz_deli = []
 
 def line(katz_deli)
-  puts "The line is currently empty."
+  if katz_deli.empty? puts "The line is currently empty."
+  else puts "The line is currently: "
 
 end
 
