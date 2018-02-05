972ea9a0efc89563bf367b96a96cba422b2f85d7
Edward Norton <edwardnorton@icloud.com>
2018-02-05 23:15:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift #shift retrieves and removes the first item called on
+    puts "Currently serving #{katz_deli.first}." #.first calls on the first person in our line/array 
+    katz_deli.shift #.shift retrieves and removes the first item called on
   end
 end
