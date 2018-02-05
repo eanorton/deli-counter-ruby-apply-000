987f2220797ae0b62c9abb5d81d521c124d7d29d
Edward Norton <edwardnorton@icloud.com>
2018-02-05 23:19:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
+  katz_deli.push(name) #.push adds an element (name) to the end of the array (katz_deli) 
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
@@ -13,7 +13,7 @@ def now_serving(katz_deli)
   if katz_deli.empty? #.empty? is checking the array to see if it contains any elements. 
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}." #.first calls on the first person in our line/array 
+    puts "Currently serving #{katz_deli.first}." #.first calls on the first person in our line/array
     katz_deli.shift #.shift retrieves and removes the first item called on
   end
 end
