7a73ccc564c59e5b1dae1c1021df1b4abc864add
Edward Norton <edwardnorton@icloud.com>
2018-02-06 00:12:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
       katz_deli.each.with_index(1) do |name, i|
         puts line_with_people << "#{i}. #{name}"
       end
-      puts line_with_people
+      puts line_with_people.each
     end
   end
 
