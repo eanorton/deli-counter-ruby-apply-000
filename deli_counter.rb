d4ef5351e330226f0994cc59bf733960dcdff86e
Edward Norton <edwardnorton@icloud.com>
2018-02-06 00:25:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,6 @@ def line(katz_deli)
     else
       line_with_people = "The line is currently:"
       katz_deli.each.with_index(1) {|person, i| puts line_with_people << " #{i}. #{person}"}
-      end
     end
   end
 
