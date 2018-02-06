f10a3c627d2bc54aaedcfdbae15c96a90001bbe2
Edward Norton <edwardnorton@icloud.com>
2018-02-06 00:21:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.empty? #.empty? is checking the array to see if it contains any elements. If true(empty) puts...
       puts "The line is currently empty."
-  else
+    else
       line_with_people = "The line is currently: "
       katz_deli.each.with_index(1) do |person, i|
         line_with_people << "#{i}. #{person}"
